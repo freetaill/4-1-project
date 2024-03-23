2ceecc8d6219203a82d972555f2d6e8ebe0ca37f
freetaill <113572099+freetaill@users.noreply.github.com>
2024-03-23 18:29:38 +0900

    1차 테스트
    스텝카운터 테스트

---
+++ Assets/jw-test_room/Screenset.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Screenset : MonoBehaviour
+{
+    [SerializeField] Canvas testCanvas;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int i_width = Screen.width;
+        int i_height = Screen.height;
+        // Canvas Scaler 컴포넌트 가져오기
+        CanvasScaler canvasScaler = testCanvas.GetComponent<CanvasScaler>();
+        // Reference Resolution 값 설정
+        canvasScaler.referenceResolution = new Vector2(i_width, i_height);
+
+        //Camera.main.orthographicSize = testCanvas.GetComponent<CanvasScaler>().referenceResolution.y / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/jw-test_room/test.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Android;
+
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    public int count = 0;
+    [SerializeField]
+    public Text stepsText;
+    [SerializeField]
+    public Text accelText;
+    [SerializeField]
+    public Text infoText;
+    [SerializeField]
+    public AndroidStepCounter asc;
+    [SerializeField]
+    public Text DebugText;
+ 
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.ACTIVITY_RECOGNITION"))
+        {
+            Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
+        }
+    }
+ 
+    private void Start()
+    {
+        if (StepCounter.current == null)
+        {
+            InputSystem.AddDevice<StepCounter>();
+        }
+ 
+        if (!LinearAccelerationSensor.current.enabled)
+        {
+            InputSystem.EnableDevice(LinearAccelerationSensor.current);
+        }
+
+        if (!StepCounter.current.enabled)
+        {
+            InputSystem.EnableDevice(StepCounter.current);
+        }
+
+        Debug.Log("Started LAS: " + LinearAccelerationSensor.current.ToString());
+    }
+ 
+ 
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!LinearAccelerationSensor.current.enabled)
+        {
+            accelText.text = "The Device is not enabled";
+            InputSystem.EnableDevice(LinearAccelerationSensor.current);
+            //DebugText.text += "Linear Acceleration was re-enabled!!\n";
+        }
+        else
+        {
+            //DebugText.text += "Currently Enabled: " + LinearAccelerationSensor.current.ToString() + "\n";
+            accelText.text = LinearAccelerationSensor.current.acceleration.ReadValue().ToString();
+        }
+ 
+        if (!StepCounter.current.enabled)
+        {
+            infoText.text = "Stepcounter is Paused";
+            InputSystem.EnableDevice(StepCounter.current);
+            //DebugText.text += "Linear Acceleration was re-enabled!!\n";
+        }
+        else
+        {
+            if (!StepCounter.current.enabled)
+            {
+                InputSystem.EnableDevice(StepCounter.current);
+            }
+            //DebugText.text += "Currently Enabled: " + StepCounter.current.ToString() + "\n";
+            count += StepCounter.current.stepCounter.ReadValue();
+            stepsText.text = "Steps: " + count.ToString();
+        }
+    }
+
+}
