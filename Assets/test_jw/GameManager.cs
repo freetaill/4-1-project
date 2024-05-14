7e47b7e3fb91aa8d80724c75a370fb0157e6aade
freetaill <113572099+freetaill@users.noreply.github.com>
2024-05-14 22:13:51 +0900

    GameManager 수정

--- Assets/test_jw/GameManager.cs
+++ Assets/test_jw/GameManager.cs
@@ -1,19 +1,46 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+public class Player
+{
+    string name;
+    int steps = 0;
+    int level = 1;
+    int exp = 0;
+    int Gold = 0;
+
+    public void insert_Data(int Get_steps)
+    {
+        steps = Get_steps;
+        //int x = steps / level;
+        //level = (int)System.Math.Log(level) + 100;
+    }
+    public void insert_coin(int Get_coins)
+    {
+        Gold += Get_coins;
+    }
+
+    public int Get_coin()
+    {
+        return Gold;
+    }
+}
+
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    public static GameManager instance;
+    public Player player = new Player();
+
     private int steps = 0;
     private int exe = 0;
 
-    public string path;
+    string path;
 
     private void Awake()
     {
-        //教臂沛 积己
         if (instance == null)
         {
             instance = this;
@@ -23,7 +50,6 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(gameObject);
-        //教臂沛 积己
 
         path = Application.dataPath + "/save";
     }
@@ -49,9 +75,4 @@ public class GameManager : MonoBehaviour
         nowSlot = -1;
         nowPlayer = new Player();
     }*/
-
-    private void getsteps(int steps)
-    {
-        this.steps = steps;
-    }
 }
