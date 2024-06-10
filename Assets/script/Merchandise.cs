b844124f6fcea2f47b6aabdeaff4b9c7ab6dc627
iaous <48446690+iaous@users.noreply.github.com>
2024-06-10 18:00:03 +0900

    스토어 수정
    1. 스토어 현재 가진 코인 나오게 수정
    2. 아이템 구매 기능 추가
    (구매시 공격력, 방어력 증가)

--- Assets/script/Merchandise.cs
+++ Assets/script/Merchandise.cs
@@ -10,12 +10,16 @@ public class Merchandise : MonoBehaviour
     public int index;
     public Image merch_img;
     public Text itemName;
-    public Text itemPrice;
+    public Text itemPriceTxt;
+    public ScrollViewManager scm;
+
+    public int itemPrice;
 
     void Awake(){
         merch_img = transform.Find("MerchandiseImage").GetComponent<Image>();
         itemName = transform.Find("ItemName").GetComponent<Text>();
-        itemPrice = transform.Find("ItemPrice").GetComponent<Text>();
+        itemPriceTxt = transform.Find("ItemPrices").GetComponent<Text>();
+        scm = GameObject.Find("Scroll View").GetComponent<ScrollViewManager>();
     }
 
 
@@ -23,10 +27,13 @@ public class Merchandise : MonoBehaviour
         this.index = index;
         merch_img.sprite = Resources.Load<Sprite>(item.spritePath);
         itemName.text=item.itemName;
-        itemPrice.text=string.Format("{0:#,###}",item.itemPrice)+"P";
-        print("hello");
+        itemPrice = item.itemPrice;
+        itemPriceTxt.text= string.Format("{0:#,###}",itemPrice)+"P";
+        print(itemPrice);   
     }
-
+    public void onClickBtn(){
+        scm.Purchase(this);
+    }
     
     
-}
+}   
--- Assets/script/scrollViewManager.cs
+++ Assets/script/scrollViewManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 public class ScrollViewManager : MonoBehaviour
@@ -10,9 +11,13 @@ public class ScrollViewManager : MonoBehaviour
     public GameObject prefab;
     public List<StoreItem> items;
     public StoreManager storeManager;
+    public Text hasCoin;
+
+    public AudioSource[] buySound;
     // Start is called before the first frame update
     void Start()
     {
+        buySound = GameObject.Find("AudioSource").GetComponents<AudioSource>();
         prefab = Resources.Load("Item") as GameObject;
         content = transform.GetChild(0).GetChild(0);
         storeManager = GameObject.Find("StoreManager").GetComponent<StoreManager>();
@@ -31,6 +36,34 @@ public class ScrollViewManager : MonoBehaviour
             newItem.transform.SetParent(content, false);
             i++;
         }
+        hasCoin.text = GameManager.instance.player.coins.ToString();
 
     }
+    public void Purchase(Merchandise merch){
+        Debug.Log(merch.itemName.text.ToString());
+        print(GameManager.instance.player.coins);
+
+        if(CheckCoin(GameManager.instance.player.coins, (int)merch.itemPrice))
+        {
+            if(merch.itemName.text.ToString() == "전설의 검"){
+                GameManager.instance.player.status[0] += 10; //공격력 증가
+                Debug.Log("공격력 증가");
+            }
+            else if(merch.itemName.text.ToString() == "전설의 방패")
+                GameManager.instance.player.status[2] += 10; //방어력 증가
+            else if(merch.itemName.text.ToString() == "전설의 신발")
+                GameManager.instance.player.status[2] += 20; //방어력 증가
+            buySound[0].Play();
+        }
+        else{
+            buySound[1].Play();
+        }
+    }
+    public bool CheckCoin(int totalGold, int itemPrice){
+        if(totalGold < itemPrice)
+            return false;
+        else   
+            return true;
+    }
 }
+
