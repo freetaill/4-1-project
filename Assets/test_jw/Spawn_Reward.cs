a21ab7bc3d011cad6efedda233a724aea8e5d269
freetaill <113572099+freetaill@users.noreply.github.com>
2024-06-25 15:57:00 +0900

    에셋 업데이트

--- Assets/test_jw/Spawn_Reward.cs
+++ Assets/test_jw/Spawn_Reward.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Spawn_Reward : MonoBehaviour
@@ -10,7 +11,18 @@ public class Spawn_Reward : MonoBehaviour
     private void Start()
     {
         Vector3 vec = new Vector3(0, 1, 0);
-        GameObject.Instantiate(Reward[0], transform.position + vec, Quaternion.identity).transform.parent = this.transform;
+        int k = Random.Range(0, Reward.Length);
+        GameObject temp =  Instantiate(Reward[k], transform.position + vec, Quaternion.identity);
+        temp.transform.parent = this.transform;
+        if (k != 0)
+        {
+            temp.AddComponent<Box_script>();
+            temp.AddComponent<AudioSource>();
+            AudioClip audio = Resources.Load<AudioClip>("Assets/에셋스토어/sound asset/wood-smash-3-170418.mp3");
+            temp.GetComponent<Box_script>().breaksound = audio;
+            AudioSource audioS = temp.GetComponent<AudioSource>();
+            audioS.clip = audio;
+        }
     }
 
     private void OnDrawGizmos()
